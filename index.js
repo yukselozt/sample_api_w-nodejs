1f4d5b306b82a714dc2f23f0eb2e2e1b1df495f8
Yüksel ÖZTÜRK <111751419+yukselozt@users.noreply.github.com>
2023-07-07 16:32:22 +0300

    (+) Add Post feature

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const { json } = require("body-parser");
 const express = require("express");
 const data = require("./data");
+const cors = require("cors");
+const { randomUUID } = require("crypto");
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.get("/", (request, response) => {
-  response.send("Hello");
+  response.send(data);
+});
+server.post("/add", (request, response) => {
+  const yeniAktor = {
+    id: randomUUID(),
+    name: request.body.celebrityName,
+    movies: request.body.celebrityMovies,
+  };
+  data.push(yeniAktor);
+  response.status(200).json(yeniAktor);
 });
 server.get("/aktorler", (request, response) => {
   response.status(200).json(data);
@@ -14,7 +26,6 @@ server.get("/aktorler", (request, response) => {
 server.get("/aktorler/:id", (request, response) => {
   /*Requestin bodysinden veri almak (POSTMAN ile)*/
   /*http://localhost:5000/aktorler/1 adresine postman ile get isteği attığımızda ya da post isteği atıldığında (örneğin frontendden) bu cevap server tarafına gelecek */
-  console.log("req.body =", request.body);
 
   /*Requestin querysinden veri almak */
   //http://localhost:5000/aktorler/1?isim=M%C3%BCnir&soyisim=%C3%96zkul&film_turu=komedi
@@ -27,6 +38,11 @@ server.get("/aktorler/:id", (request, response) => {
     response.status(200).send("Aradığınız aktör bulunamadı");
   }
 });
+
+server.listen(5000, () => {
+  console.log("5000 Portuna gelen istekler dinleniyor...");
+});
+
 
 server.listen(5000, () => {
   console.log("5000 Portuna gelen istekler dinleniyor...");
